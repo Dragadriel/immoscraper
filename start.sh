fda7a81d97872afd4dda0ab0318b2738711724a1
Dragadriel <evangelion_01@outlook.de>
2025-05-13 22:00:43 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
-
-echo "Installiere Chromium für Playwright..."
+echo "▶ Installiere Chromium für Playwright..."
 playwright install chromium
 
-echo "Starte Immobotscript..."
+echo "🚀 Starte Python-Skript"
 python main.py
